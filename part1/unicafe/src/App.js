f08d941f5b432d4a62c631356a0fb4794ea8e14f
Eemil Asp <Eemil.aspholm@gmail.com>
2022-05-23 19:39:31 +0300

    commit

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = ({handleClick, text}) => {
+  return(
+  <button onClick={handleClick}>{text}</button>
+  )
 }
 
-export default App;
+const Stat = ({type, value, symbol}) => 
+<>{type}{value}{symbol}</>
+
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const average = (good - bad)/total
+  const positive = (good/total)*100
+
+  if(total === 0){
+  return(
+    <div>
+      <p>No feedback given</p>
+    </div>
+  )}
+  return(
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td><Stat type="good" /></td>
+            <td><Stat value={good} symbol="" /></td>
+          </tr>
+          <tr>
+            <td><Stat type="neutral" /></td>
+            <td><Stat value={neutral} symbol="" /></td>
+          </tr>
+          <tr>
+            <td><Stat type="bad" /></td>
+            <td><Stat value={bad} symbol="" /></td>
+          </tr>
+          <tr>
+            <td><Stat type="total" /></td>
+            <td><Stat value={total} symbol="" /></td>
+          </tr>
+          <tr>
+            <td><Stat type="average" /></td>
+            <td><Stat value={average} symbol="" /></td>
+          </tr>
+          <tr>
+            <td><Stat type="positive" /></td>
+            <td><Stat value={positive} symbol="%" /></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () =>{
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () =>{
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () =>{
+    setBad(bad + 1)
+  }
+
+  return(
+      <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() =>handleGoodClick()} text='good'/>
+      <Button handleClick={() =>handleNeutralClick()} text='neutral'/>
+      <Button handleClick={() =>handleBadClick()} text='bad'/>
+      <h2>statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
+      </div>
+       );
+}
+
+export default App
