e61456e1b423232663762533d920c5e82880ba49
Eemil Asp <Eemil.aspholm@gmail.com>
2022-09-01 13:44:48 +0300

    commit

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,55 +1,68 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+
 import Persons from "./components/Person"
 import Filter from "./components/Filter"
 import Personform from "./components/PersonForm"
 
 const App = () => {
+ 
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
+  
+
   const [shownPersons, setPersonsToShow] = useState([
       { name: 'Arto Hellas', number: '040-123456' },
       { name: 'Ada Lovelace', number: '39-44-5323523' },
       { name: 'Dan Abramov', number: '12-43-234345' },
       { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
+
+  useEffect(() =>
+    console.log('effect')
+  )
+
   const [newFilter, setNewFilter] = useState('');
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
+  // If filter changes
   const handleFilterChange = (event) => {
-    setNewFilter(event.target.value)
-    setPersonsToShow(persons.filter((person) =>
-    person.name.toLowerCase().search(newFilter.toLowerCase()) !== -1));
+    setNewFilter(event.target.value) //Input as filter
+    setPersonsToShow(persons.filter((person) => //set filter to shownPersons array
+    person.name.toLowerCase().search(newFilter.toLowerCase()) !== -1));//show all persons that match the letters given from input
   };
 
   const handleNameChange = (event) =>{
-    setNewName(event.target.value)
+    setNewName(event.target.value) //new name from input
   }
+
   const handleNumberChange = (event) =>{
-    setNewNumber(event.target.value)
+    setNewNumber(event.target.value) //new number from input
   }
 
   const addContact = (event) => {
-    event.preventDefault()
-    
-    const phoneBookObject = {
+    event.preventDefault(); //prevents the website from loading when clicking submit
+
+    const phoneBookObject = { //save new contact as object
       name: newName,
       number: newNumber,
       id: persons.length + 1,
     }
     
-    if(persons.filter(person => person.name === newName).length > 0)
+    if((persons.filter(person => person.name === newName).length > 0) || newName.length === 0 ) // test if the persons name is already added to contacts or no name added
     {
       alert( newName + " is already added to phonebook")
-    }else{
-    setPersons(persons.concat(phoneBookObject))
-    setPersonsToShow(persons.concat(phoneBookObject))
-    setNewName('')
-    setNewNumber('')
+    }else{//if not save the contact info to persons array and shownPersons array
+    setPersons(persons.concat(phoneBookObject));
+    setPersonsToShow(persons.concat(phoneBookObject));
+    event.target.reset();
+    setNewName("");
+    setNewNumber("");
     }
   }
 
@@ -59,11 +72,16 @@ const App = () => {
       <div>
           <Filter value={newFilter} onChange={handleFilterChange}></Filter>
       </div>
-      <h3>Add a new</h3>
+      <h3>Add a new contact</h3>
       <div>
-          <Personform addContact={addContact} name={newName} number={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}></Personform>
+          <Personform addContact={addContact}
+                      name={newName}
+                      number={newNumber}
+                      handleNameChange={handleNameChange}
+                      handleNumberChange={handleNumberChange}>
+                      </Personform>
       </div>
-      <h3>Numbers</h3>
+      <h3>Contacts</h3>
       <div>
           <Persons shownPersons={shownPersons}></Persons>
       </div>
