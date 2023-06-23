2ecac80b2e3b1eba585078a881aa14a7698feab2
Eemil Asp <Eemil.aspholm@gmail.com>
2023-06-23 14:11:41 +0300

    assingment 4.1 and 4.2

---
+++ part4/index.js
@@ -0,0 +1,10 @@
+const app = require('./App')
+const config = require('./utils/config')
+const http = require('http');
+const logger = require('./utils/logger')
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
