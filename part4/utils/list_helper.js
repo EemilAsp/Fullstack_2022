877606621f8951039c8694649450b528f115af90
Eemil Asp <Eemil.aspholm@gmail.com>
2023-06-23 15:50:32 +0300

    4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,12 @@
 const dummy = (blogs) => {
     return 1;
   }
+
+const totalLikes = (blogs) =>{
+    return(blogs.reduce((total, blog) => total + blog.likes, 0))
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
