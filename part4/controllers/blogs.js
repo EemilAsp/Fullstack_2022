14e59ab1e22460a9bc44e7da88f46c04094724ee
Eemil Asp <Eemil.aspholm@gmail.com>
2023-06-26 19:41:38 +0300

    4.22 done and working smooth

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -29,7 +29,6 @@ blogRouter.post('/', middleware.userExtractor, async (request, response) => {
       user: user._id
     })
 
-
     const savedBlog = await newBlog.save()
     user.blogs = user.blogs.concat(savedBlog.id)
     await user.save()
@@ -38,27 +37,33 @@ blogRouter.post('/', middleware.userExtractor, async (request, response) => {
 })
 
 blogRouter.delete('/:id', middleware.userExtractor, async (request, response) =>{
-    //try{
+    try{
     const blog = await Blog.findById(request.params.id)
-    console.log("blog", blog.user)
-    console.log("params id", request.params.id)
     const user = await request.user
-    console.log(user._id.toString())
-    console.log(blog.user._id.toString())
+    console.log(user)
+    console.log(blog)
     if(blog.user.toString() === user._id.toString()){ // test if blog user id and request user id match
       await Blog.findByIdAndRemove(request.params.id) // delete if matches
-      response.status(204).end()
+      response.status(200).end("The blog has been succesfully deleted from the database")
     }
-    //}catch{
+    }catch{
       response.status(401).json({error: 'Only Blog writer can delete blog for database'})
-    //}
+    }
 })
 
 blogRouter.put('/:id', middleware.userExtractor, async (request, response) =>{
-    const blog = request.body
-    const user = request.user
+    try{
+    const blog = await request.body
+    const user = await request.user
+    const oldBlog = await Blog.findById(request.params.id)
 
-    const updatedBlog = {
+    console.log("Uusi blogi: ", blog)
+    console.log("Vanha blogi ID:n perusteella: ",oldBlog)
+    console.log("Käyttäjä", user)
+
+    if(oldBlog.user.toString() === user._id.toString()){ // test if blog user id and request user id match
+
+    const updatedBlog = { // Edit blog it editor and blog's user id matches
         title: blog.title,
         author: blog.author,
         url: blog.url,
@@ -68,8 +73,12 @@ blogRouter.put('/:id', middleware.userExtractor, async (request, response) =>{
 
     await Blog.findByIdAndUpdate(request.params.id, updatedBlog,
       { new: true})
-    
+
     response.status(200).json(updatedBlog)
+    }
+    }catch{
+      response.status(401).json({error: 'Only Blog writer can edit blog inside the database'})
+    }
 })
 
 
