dbf9e3646ad16199934e49fba41bde09c18908c0
Eemil Asp <Eemil.aspholm@gmail.com>
2023-07-03 16:21:43 +0300

    5.7 done

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,11 +1,42 @@
+import React, {useState} from 'react'
 
+const Blog = ({blog}) => {
 
-const Blog = ({blog}) => 
+          const [showInfo, setInfo] = useState(false)
+          const hideWhenVisible = { display: showInfo ? 'none' : '' }
+          const showWhenVisible = { display: showInfo ? '' : 'none' }
 
-(
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+          const blogStyle = {
+            paddingTop: 10,
+            paddingLeft: 2,
+            border: 'solid',
+            borderWidth: 1,
+            marginBottom: 5
+          }
+          
+          const addLike = () => {
+            //TODO
+            //PUT API call +1 like
+            console.log("like + 1")
+          }
+
+          return(
+            <div style={blogStyle}>
+              <div style={hideWhenVisible}> 
+                {blog.title} {blog.author}
+                <button onClick={() => setInfo(true)}>show</button>
+              </div>
+              <div style={showWhenVisible}>
+                <p>{blog.title} {blog.author} 
+                <button onClick={() => setInfo(false)}>hide</button>
+                </p>
+                <p>{blog.url}</p>
+                <p>{blog.likes}
+                <button onClick={() => addLike()}>Like</button></p>
+                <p>{blog.user.name}</p>
+              </div>
+            </div>
+            )
+}
 
 export default Blog
