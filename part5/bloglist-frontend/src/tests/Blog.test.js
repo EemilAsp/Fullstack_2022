dcbd65af0dee5c410795fcb085bd52adb752df5a
Eemil Asp <Eemil.aspholm@gmail.com>
2023-07-05 17:29:00 +0300

    5.14 done + refactoring tests

--- part5/bloglist-frontend/src/tests/Blog.test.js
+++ part5/bloglist-frontend/src/tests/Blog.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from '../components/Blog'
 
 const testUser = {
-  username: 'Teemu testaaja',
+  username: 'Teemuntesti',
   name: 'Teemu',
 }
 
@@ -13,18 +14,37 @@ const blog = {
   author: 'Teemu testaaja',
   url: 'https://www.testi.fi/testi',
   likes: 0,
-  user: testUser,
+  user: {
+    username: 'Teemuntesti',
+    name: 'Teemu'
+  },
 }
 
-beforeEach(() => {
-  render(<Blog blog={blog} user={testUser} />)
-})
 
-describe('Basic test', () => {
-  test('renders title', () => {
+describe('Basic test', () => { //5.13
+  test('renders title', async () => {
+    render(<Blog blog={blog} user={testUser} />)
     const title = screen.getByText('Component testing is done with react-testing-library')
     const author = screen.getByText('Teemu testaaja')
     expect(title).toBeDefined()
     expect(author).toBeDefined()
   })
+
+  test('All info showed', async () => { //5.14
+    render(<Blog blog={blog} user={testUser} />)
+    const us = userEvent.setup()
+    const button = screen.getByText('Component testing is done with react-testing-library')
+    await us.click(button)
+    const title = screen.getByText('Component testing is done with react-testing-library')
+    const author = screen.getByText('Teemu testaaja')
+    const url = screen.getByText('Teemu testaaja')
+    const likes = screen.getByText('0')
+    const username = screen.getByText('Teemu')
+    expect(title).toBeDefined()
+    expect(author).toBeDefined()
+    expect(url).toBeDefined()
+    expect(username).toBeDefined()
+    expect(likes).toBeDefined()
+  })
+
 })
