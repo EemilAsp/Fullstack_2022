34820938376db54c589c21740387fcd6f28db537
Eemil Asp <Eemil.aspholm@gmail.com>
2023-07-05 20:47:15 +0300

    5.18 done

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,37 @@
 describe('Blog app', function() {
+
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+    const testUser = {
+      name: 'Teemu Testaaja',
+      username: 'Teemuntesti',
+      password: 'Teemutestaa321'
+    }
+
+    cy.request('POST', 'http://localhost:3003/api/users/',testUser)
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     cy.contains('log in').click()
   })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('Teemuntesti')
+      cy.get('#password').type('Teemutestaa321')
+      cy.get('#loginbtn').click()
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong')
+      cy.get('#loginbtn').click()
+      cy.get('.error').contains('Invalid credentials')
+    })
+  })
+
 })
