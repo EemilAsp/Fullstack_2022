c1a90e7b946b6302d4742fc75f4540e90110b3d3
Eemil Asp <Eemil.aspholm@gmail.com>
2023-07-10 10:48:41 +0300

    6.5 done

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -4,6 +4,7 @@ import { addLike, createAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
+  const sortedAnecdotes = [...anecdotes].sort((a , b) => b.votes - a.votes)
   const dispatch = useDispatch()
 
   const addAnecdote = (event) => {
@@ -16,7 +17,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {sortedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
