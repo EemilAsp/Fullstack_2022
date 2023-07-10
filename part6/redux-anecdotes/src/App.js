201ce75ae33d0a1bffd5262687b0262022fcdc26
Eemil Asp <Eemil.aspholm@gmail.com>
2023-07-10 10:26:35 +0300

    6.3 done

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { addLike } from './reducers/anecdoteReducer'
+
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => dispatch(addLike(anecdote.id))}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form>
+        <div><input /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
