5e55727e2354949a901db754f8d8c985d1eee1a4
Eemil Asp <Eemil.aspholm@gmail.com>
2023-07-12 12:34:14 +0300

    7.7 done

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { useCountry } from './hooks'
+
+const useField = (type) => { // useField Hook
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const Country = ({ country }) => {
+  if(!country){
+    return null
+  }else if (!country.found) {
+    return (
+        <div>
+          not found...
+        </div>
+        )}
+  return (
+    <div>
+      <h3>{country.data.name.common} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.name.common}`}/>  
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
---
+++ part7/country-hook/src/hooks/index.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() =>{
+    if(name){
+        axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+        .then((response) => {
+            setCountry({found: true, data: response.data})
+        })
+        .catch((error) => {
+            setCountry({found: false})
+        })
+    }
+  }, [name])
+
+  return country
+}
