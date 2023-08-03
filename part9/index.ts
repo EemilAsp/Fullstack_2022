eca06c36153800cb3bd2e41f65cdc23cb50420bf
Eemil Asp <Eemil.aspholm@gmail.com>
2023-08-03 17:13:41 +0300

    added isNotNumber util

--- part9/index.ts
+++ part9/index.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import calculateBmi from "./bmiCalculator"
+import isNotNumber from "./utils"
 const app = express()
 
 app.get("/bmi", (req, res) => {
   const height = Number(req.query.height)
   const weight = Number(req.query.weight)
-  if (isNaN(height) || isNaN(weight)) {
+  if (isNotNumber(height) || isNotNumber(weight)) {
     res.status(400).json({ error: "malformatted parameters" })
   } else {
     const results = {
