57b9c026121abcd6eaa62ef2a4d0a93462356566
Eemil Asp <Eemil.aspholm@gmail.com>
2023-08-03 20:04:37 +0300

    refactored

--- part9/index.ts
+++ part9/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express"
+import express from "express"
 import calculateBmi from "./bmiCalculator"
 import isNotNumber from "./utils"
 import calculateExercise from "./exerciseCalculator"
