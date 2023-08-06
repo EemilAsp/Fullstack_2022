9044798c9dc9be9588e4b5110765a7e390ea56d3
Eemil Asp <Eemil.aspholm@gmail.com>
2023-08-06 10:50:15 +0300

    removed unnecessary imports

--- part9/courses-app/src/components/Content.tsx
+++ part9/courses-app/src/components/Content.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import Part from "./Part"
 import { CoursePart } from "../types"
 
--- part9/courses-app/src/components/Total.tsx
+++ part9/courses-app/src/components/Total.tsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 interface Part {
   name: string
   exerciseCount: number
