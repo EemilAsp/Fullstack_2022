d14b581ea16add4a3ddd7019c149d09bc9783b12
Eemil Asp <Eemil.aspholm@gmail.com>
2023-08-06 11:31:53 +0300

    tsconfig edited

--- part9/flightDiary/diary-backend/src/types.ts
+++ part9/flightDiary/diary-backend/src/types.ts
@@ -1,26 +1,26 @@
 export enum Weather {
-  Sunny = 'sunny',
-  Rainy = 'rainy',
-  Cloudy = 'cloudy',
-  Stormy = 'stormy',
-  Windy = 'windy',
+  Sunny = "sunny",
+  Rainy = "rainy",
+  Cloudy = "cloudy",
+  Stormy = "stormy",
+  Windy = "windy",
 }
 
 export enum Visibility {
-  Great = 'great',
-  Good = 'good',
-  Ok = 'ok',
-  Poor = 'poor',
+  Great = "great",
+  Good = "good",
+  Ok = "ok",
+  Poor = "poor",
 }
 
 export interface DiaryEntry {
-  id: number;
-  date: string;
-  weather: Weather;
-  visibility: Visibility;
-  comment: string;
+  id: number
+  date: string
+  weather: Weather
+  visibility: Visibility
+  comment: string
 }
 
-export type NewDiaryEntry = Omit<DiaryEntry, 'id'>;
+export type NewDiaryEntry = Omit<DiaryEntry, "id">
 
-export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>;
+export type NonSensitiveDiaryEntry = Omit<DiaryEntry, "comment">
