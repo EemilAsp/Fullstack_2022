13e1f382a87402dfc712c8ca8df90a7879ce66b4
Eemil Asp <Eemil.aspholm@gmail.com>
2023-08-09 17:40:52 +0300

    9.22 done

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -6,6 +6,11 @@ export enum Gender {
   Other = "other",
 }
 
+export interface Discharge {
+  date: string
+  criteria: string
+}
+
 export interface Patient {
   id: string
   name: string
@@ -14,6 +19,30 @@ export interface Patient {
   gender: Gender
   occupation: string
   entries: Entry[]
+}
+
+export interface BaseHealthCareEntry {
+  id: string
+  date: string
+  specialist: string
+  diagnoseCodes: string[]
+  description: string
+}
+
+export interface HospitalEntry extends BaseHealthCareEntry {
+  type: "Hospital"
+  discharge: {
+    date: string
+    criteria: string
+  }
+}
+
+export interface OccupationalHealthcareEntry extends BaseHealthCareEntry {
+  type: "OccupationalHealthcare"
+  sickLeave: {
+    startDate: string
+    endDate: string
+  }
 }
 
 export type NonSensitivePatientEntry = Omit<Patient, "ssn" | "entries">
